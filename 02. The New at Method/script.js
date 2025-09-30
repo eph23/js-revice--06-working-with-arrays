b70cc8d9f48b2b4e84976d3e4845eaa0170f978f
ephraim <ephraims.818@gmail.com>
2025-10-01 04:19:29 +0600

    JS-REVICE--COMPLETE-JS---ARRAYS - 02. The New at Method

---
+++ 02. The New at Method/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+console.log(arr[arr.length - 1]);
+console.log(arr.at(arr.length - 1));
+console.log(arr.at(-1));
+
